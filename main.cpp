3bd95525940dd0d20279990a4dd7225b66a6f637
stokstaartje123 <staartjesstonks1487@gmail.com>
2024-04-24 12:06:54 +0200

    added the way to use xbee.println instead of Serial1.println

--- main.cpp
+++ main.cpp
@@ -4,4 +4,7 @@
 int main() {
   cout << "Program is Initializing"
   // Add more code after here, and switch above to write to Serial.
+
+  auto& xbee = Serial1;
+  // While using the same Serial class as the arduino IDE does, add this to be able to do xbee.println() instead of Serial1.println()
 }
