bab9f47675a112c0a4cbf7464c02c83560e74dde
Stetsed <stetsed@selfhostable.net>
2024-05-05 10:44:36 +0200

    Modified Naming Convention, Documentation
    Updated naming for the private variable from _proxSensor to
    pvt_proxSensor due to C++ standard reserving _ for global namespace as
    per ISO/IEC-14882-03.
    
    Modified documentation to provide more information on working and usage
    of functions and classes within proxSensor class and functions. Also
    finished moving debug to only be active with debug boolean set to true,
    so number of sensors and data from Sensors.

--- work/sensor/proximity/proxsensor.cpp
+++ work/sensor/proximity/proxsensor.cpp
@@ -3,24 +3,35 @@
 #include "HardwareSerial.h"
 #include <Arduino.h>
 
-// Initialize for Global Variable
+/** proxSensor constructor without input 
+ * Only used within global variable decleration, not used outside. 
+*/
 proxSensor::proxSensor() {
 }
 
-// Initialize while giving it the ZumoProxmitySensor which has to be preinitialized
-proxSensor::proxSensor(Zumo32U4ProximitySensors a) : _proxSensor(a) {
+/** proxSensor constructor with Proximity Sensor Instance
+ * Get's given an instance of Zumo32U4ProximitySensors, due to issues when
+ * initializing inside of the Constructor itself.
+*/
+proxSensor::proxSensor(Zumo32U4ProximitySensors a) : pvt_proxSensor(a) {
 
 }
 
+/** Perform a basic read
+ *  Returns back Boolean if >= 50(?)cm in distance, otherwise return false.
+ *  If debug global var is true, shall return number of sensors active and the raw data to Serial1 
+*/
 bool proxSensor::basicReadClose() {
-  //Serial1.println("Number of Sensors Active: " + String(_proxSensor.getNumSensors()));
-  _proxSensor.read();
-  int leftled = _proxSensor.countsFrontWithLeftLeds();
-  int rightled = _proxSensor.countsFrontWithRightLeds();
+  if (debug) {
+    Serial1.println("Number of Sensors Active: " + String(pvt_proxSensor.getNumSensors()));
+  }
+  pvt_proxSensor.read();
+  int leftled = pvt_proxSensor.countsFrontWithLeftLeds();
+  int rightled = pvt_proxSensor.countsFrontWithRightLeds();
 
   if (debug) {
-  Serial1.println("Data L: " + String(leftled));
-  Serial1.println("Data R: " + String(rightled));
+    Serial1.println("Data L: " + String(leftled));
+    Serial1.println("Data R: " + String(rightled));
   }
 
   if (leftled > 4 && rightled > 4){
@@ -32,6 +43,7 @@ bool proxSensor::basicReadClose() {
 
 }
 
+// Advanced Read, currently unused, possibly will return int of distance if able to acces > 1 prox sensor and/or read analog data.
 int proxSensor::deepRead() {
   Serial1.println("This is not yet implemented,future me's problem");
 }
--- work/sensor/proximity/proxsensor.h
+++ work/sensor/proximity/proxsensor.h
@@ -1,19 +1,23 @@
 #include "Zumo32U4ProximitySensors.h"
 
+/** Class for reading the Proximity Sensor and returning Usable Data
+ * To-do: Subclass of eventual "Sensor" baseclass, implement later.
+*/
 class proxSensor {
 public:
   // Empty Initialize
   proxSensor();
 
-  // Pass through sensor
+  // Initialize Class with an instacen of the Proximity Sensor Class Zumo32U4ProximitySensors
   proxSensor(Zumo32U4ProximitySensors);
 
-  // Basic read, 
+  // Basic read, returns boolean if closer than 50(?)cm infront of the Zumo
   bool basicReadClose();
+  // Advanced Read, currently unused, possibly will return int of distance if able to acces > 1 prox sensor and/or read analog data.
   int deepRead();
 
 private:
-  Zumo32U4ProximitySensors _proxSensor;
+  Zumo32U4ProximitySensors pvt_proxSensor;
 };
 
 #include "proxsensor.cpp"
