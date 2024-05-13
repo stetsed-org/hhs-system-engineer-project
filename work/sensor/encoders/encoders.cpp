dd585f86e3c67923b5f0181a076e2e6d20e1b855
o3-o3 <167069651+o3-o3@users.noreply.github.com>
2024-05-13 11:44:40 +0200

    testing pointers

--- work/sensor/encoders/encoders.cpp
+++ work/sensor/encoders/encoders.cpp
@@ -6,15 +6,15 @@ CompatibleEncoders::CompatibleEncoders(){
 }
 
 uint32_t CompatibleEncoders::ReadLeft(){
-    EncoderValue[1] += getCountsAndResetLeft();
-    return EncoderValue[1];
+    EncoderValue[0] += getCountsAndResetLeft();
+    return EncoderValue[0];
 }
 
 uint32_t CompatibleEncoders::ReadRight(){
-    EncoderValue[2] += getCountsAndResetRight();
-    return EncoderValue[2];
+    EncoderValue[1] += getCountsAndResetRight();
+    return EncoderValue[1];
 }
 
 uint32_t CompatibleEncoders::Read(){
-    return (EncoderValue[1],EncoderValue[2]);
+    return *EncoderValue;
 }
