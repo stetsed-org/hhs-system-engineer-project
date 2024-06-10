0498571bb14c5e859ea9fc3c02b092fbbe3130f8
StarBerzt <sigert360m@gmail.com>
2024-06-10 06:50:25 +0200

    Added doxygen comments

--- work/motor/MotorController.cpp
+++ work/motor/MotorController.cpp
@@ -2,20 +2,37 @@
 #include "MotorController.h"
 
 
-
+//! \brief MotorController constructor
 MotorController::MotorController() {}
 
-
+/*! \brief Sets the speeds for both motors.
+ *
+ *  This function constrains the speed values to be within the range -350 to 350
+ *  and then sets the speeds using the Zumo32U4Motors instance.
+ *  
+ *  \param newLeft Speed for the left motor.
+ *  \param newRight Speed for the right motor.
+ */
 void MotorController::setSpeed(int newLeft, int newRight){
   newLeft = constrain(newLeft, -350, 350);
   newRight = constrain(newRight, -350, 350);
   motors.setSpeeds(newLeft, newRight);
 }
 
+/*! \brief Reads the current speeds of the motors.
+ *
+ *  This function reads the current speeds and returns them.
+ *  
+ *  \return A motorSpeeds struct containing the speeds of the left and right motors.
+ */
 motorSpeeds MotorController::read() {
   return { OCR1B, OCR1A };
 }
 
+/*! \brief Stops both motors.
+ *
+ *  This function stops both motors by setting their speeds to 0.
+ */
 void MotorController::stop(){
   motors.setSpeeds(0, 0);
 }
--- work/motor/MotorController.h
+++ work/motor/MotorController.h
@@ -1,25 +1,45 @@
-
 #ifndef MOTORCONTROLLER_H
 #define MOTORCONTROLLER_H
 
 #include "../headers.hpp"
 
+/*! \struct motorSpeeds
+ *  \brief A structure to hold the motor speeds.
+ */
 struct motorSpeeds {
   uint16_t left, right;
 };
 
+
+/*! \class MotorController
+ *  \brief A class to control the Zumo32U4 motors.
+ * 
+ *  This class inherits from Sensor and provides methods to control the motor
+ *  speeds, read the current speeds, and stop the motors.
+ */
 class MotorController : public Sensor {
   private:
-    Zumo32U4Motors motors;
+    Zumo32U4Motors motors; /*!< Instance of Zumo32U4Motors to control the motors */
 
   public:
+      /*! \brief Constructor for MotorController. */
     MotorController();
+
+     /*! \brief Reads the current speeds of the motors.
+     *  \return A motorSpeeds struct containing the speeds of the left and right motors.
+     */
     motorSpeeds read();
-    void setSpeed(int, int);
+
+    /*! \brief Sets the speeds for both motors.
+     *  \param newLeft Speed for the left motor.
+     *  \param newRight Speed for the right motor.
+     */
+    void setSpeed(int newLeft, int newRight);
+
+    /*! \brief Stops both motors. */
     void stop();
 };
 
-#include "MotorController.cpp"
+//#include "MotorController.cpp"
 
-#endif
-
+#endif // MOTORCONTROLLER_H
