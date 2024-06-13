e23fb992f988404a72f3aaf183321910a9e37af1
ducoa <ducoalsem@gmail.com>
2024-06-13 22:51:17 +0200

    Added Dutch commenting in a better way than it previously was

--- work/sensor/Linesensors/linesensors.cpp
+++ work/sensor/Linesensors/linesensors.cpp
@@ -2,29 +2,39 @@
 #include "linesensors.h"
 
 
-//Constructor of linesensor class, configuring a few settings for correct use within the object.
+/**
+ * Constructor voor het maken van een object van linesensors. Tevens wordt hier useEmitters op true gezet. 
+ * Deze kan als false worden ingesteld door de code aan te passen, mochten we de emitters toch niet willen gebruiken. 
+ */
 linesensors::linesensors():lineSensorsLibrary(),useEmitters(true){
-  
   lineSensorsLibrary.initFiveSensors();
-  Serial1.println("linesensor object built"); 
+  //Serial1.println("linesensor object built"); // debug line, in comment geplaats wanneer deze niet gebruikt wordt
 }
 
-//Function to read the value of each linesensor, mainly to use within this class.
+/**
+ * Functie die zorgt dat via de methode van de library de values array die gevuld/overschreven wordt met de meest recente lijnsensor waardes.
+ */
 void linesensors::readValues(){
   lineSensorsLibrary.read(values, useEmitters);
 }
-
+/**
+ * Functie die de values array returnt om deze buiten de klasse beschikbaar te stellen.
+ */
 unsigned int* linesensors::gibValues(){
   return values;
 }
 
-//Function to print the value of each linesensor in a format used by the dashboard.
+/**
+ * Functie om de waardes uit de values array via de Xbee te printen.
+ * De prints staan in een format dat overeenkomt met de instellingen van Robbert's dashboard,
+ * hierdoor kan deze eventueel gebruikt worden om de waardes in een grafiek te kunnen bekijken ipv in de serial monitor.
+ */
 void linesensors::serialPrintValues(){
-  Serial1.println("L"); // Begin of linesensor measurement values
+  Serial1.println("L"); // Duidt het begin van de reeks waardes aan, wordt tevens gebruikt door het dashboard om de waardes op de juiste manier in grafieken te tonen.
   Serial1.println(values[0]);
   Serial1.println(values[1]);
   Serial1.println(values[2]);
   Serial1.println(values[3]);
   Serial1.println(values[4]);
-  Serial1.println("l"); // end of these measurement values. These are mainly important for use in the dashboard, to correctly insert the values.
+  Serial1.println("l"); // Duidt het einde van de reeds waardes aan, wordt tevens gebruikt door het dashboard om de waardes op de juiste manier in grafieken te tonen.
 }
--- work/sensor/Linesensors/linesensors.h
+++ work/sensor/Linesensors/linesensors.h
@@ -1,19 +1,20 @@
 #include "../../headers.hpp"
 
 
-
+/**
+ * De linesensors klasse zorgt ervoor dat met behulp van de Zumo library de 5 lijnsensoren aan de voorkant van de Zumo kunnen worden uitgelezen.
+ * De uitgelezen waardes kunnen daarna opgevraagt of direct over de Xbee geprint worden door middel van de publieke functies.
+ */
 class linesensors : public Zumo32U4LineSensors{
     public:
-        linesensors();
-        void readValues();
-        void serialPrintValues();
-        unsigned int* gibValues();
+        linesensors(); // Constructor
+        void readValues(); // Leest alle lijnsensoren uit en slaat de waardes op in de values array.
+        void serialPrintValues(); // Print de waardes uit de values array in een vorm die bruikbaar is met Robbert's dashboard voor het eventueel debuggen/calibreren.
+        unsigned int* gibValues(); // returnd de values array voor het gebruik elders buiten de klasse.
 
     private:
-        //sensorReference Zumo32U4LineSensors; //ignore problem, for now... ;)
-        Zumo32U4LineSensors lineSensorsLibrary; //renamed to clearly state the function comes from the library
-        bool useEmitters;
-        //int sensorAmount; //Currently not in use, might delete later if it remains unused
-        unsigned int values[5];
-        int pins[5];
+        Zumo32U4LineSensors lineSensorsLibrary; //Object van de library voor het gebruik van functies uit deze library.
+        bool useEmitters; // Bool die gebruikt kan worden om de IR emitters aan of uit te zetten. Deze dient alleen als instelling en er is geen functie om deze aan te passen.
+        unsigned int values[5]; // array die de meest recente waardes die uitgelezen zijn van de lijnsensoren vast houdt.
+        int pins[5]; // array die de pinnen vast houdt waaraan de lijnsensoren zijn bevestigd.
 };
